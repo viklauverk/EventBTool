225b7cdfc2deeed9277ca7b35e28fada6665270f
Fredrik Öhrström <oehrstroem@gmail.com>
2024-07-14 15:18:03 +0200

    Re-add Version.java

---
+++ src/main/java/com/viklauverk/eventbtools/core/Version.java
@@ -0,0 +1,5 @@
+package com.viklauverk.eventbtools.core;
+public class Version {
+    public static String version = "AntlrReproducer_v1.5.0-59-g93609de with local changes"; 
+    public static String commit = "93609de4d44f0103bceab6683f2fdeb69a3e7a27 with local changes"; 
+}
